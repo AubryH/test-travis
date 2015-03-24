32d023d3948139df4fd3f4ae42922b1b7bf7962a
AubryH <aubryh@theodo.fr>
2015-03-24 14:59:09 +0100

    add KATA 1 project + phpunit xml

---
+++ src/Kata.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dasaule
+ * Date: 24/03/15
+ * Time: 10:18
+ */
+
+class Kata {
+    /**
+     * Sum the numbers
+     * @param $numbers
+     * @return int
+     */
+    const BEGIN_TOK_CUSTOM_DELIMETER = "//";
+    const END_TOK_CUSTOM_DELIMETER = "\n";
+
+    const BEGIN_C_DELIM = "[";
+    const END_TOK_C_DELIM = "]";
+
+    const BASICDELIMETERS = "[,][\n]";
+
+    const MAX_SUMMABLE_INT = 1000;
+
+
+    public static function add($numbers){
+        $input = $numbers;
+        $delimeters = self::BASICDELIMETERS;
+
+        if(substr($input,0,strlen(self::BEGIN_TOK_CUSTOM_DELIMETER)) == self::BEGIN_TOK_CUSTOM_DELIMETER){
+            $input = substr($input,strlen(self::BEGIN_TOK_CUSTOM_DELIMETER));
+
+            $nlindex = strpos($input, self::END_TOK_CUSTOM_DELIMETER);
+            if ($nlindex === false) {
+                return false;
+            }
+            $delimitersInputs = substr($input, 0, $nlindex);
+
+            if($delimitersInputs[0] !== self::BEGIN_C_DELIM){
+                $delimeters = $delimeters . self::BEGIN_C_DELIM . $delimitersInputs . self::END_TOK_C_DELIM;
+            }else{
+                $delimeters = $delimeters . $delimitersInputs ;
+            }
+        }
+
+
+        $delimeters = substr($delimeters, strlen(self::BEGIN_C_DELIM), strlen($delimeters)-strlen(self::BEGIN_C_DELIM)-strlen(self::END_TOK_C_DELIM));
+        $delTab = explode("][", $delimeters);
+
+
+
+        $input = str_replace($delTab, $delTab[0], $input);
+
+
+        $nums = explode(",", $input);
+        $negative = array();
+        $numTosum = array();
+        foreach($nums as $num){
+            if(substr($num,0,1) == "-"){
+                array_push($negative, $num);
+            }
+            $num = intval($num);
+            if($num > self::MAX_SUMMABLE_INT){
+                array_push($numTosum, 0);
+            }else{
+                array_push($numTosum, $num);
+            }
+        }
+
+        if(sizeof($negative)>0){
+            throw new Exception("negatives not allowed '" . implode("' '", $negative) . "'");
+        }
+        return array_sum($numTosum);
+  }
+}
---
+++ tests/KataTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dasaule
+ * Date: 24/03/15
+ * Time: 10:20
+ */
+require_once 'vendor/phpunit/phpunit/src/Framework/TestCase.php';
+require_once 'src/Kata.php';
+
+class KataTest extends PHPUNIT_Framework_TestCase{
+
+
+    /**
+     *
+     */
+    public function testAddEmptyString(){
+        $emptyString = "";
+        $this->assertEquals(0, Kata::add($emptyString));
+    }
+
+    public function testAddOneNumberString(){
+        $fourtytwo = "42";
+        $this->assertEquals(42, Kata::add($fourtytwo));
+    }
+
+    public function testAddTwoNumberString(){
+        $fourtytwo = "21,21";
+        $this->assertEquals(42, Kata::add($fourtytwo));
+    }
+
+    public function testAddLotOfNumberString(){
+        $fourtytwo = "1,1,1,1,1,5,10,20,2";
+        $this->assertEquals(42, Kata::add($fourtytwo));
+    }
+
+    public function testAddWithNewLine(){
+        $six = "1\n2,3";
+        $this->assertEquals(6, Kata::add($six));
+    }
+
+    public function testAddWithCustomDelim(){
+        $five = "//;\n2;3";
+        $this->assertEquals(5, Kata::add($five));
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage negatives not allowed '-3'
+     */
+    public function testNegative(){
+        $neg = "1,-3,3";
+
+        $result = Kata::add($neg);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage negatives not allowed '-1' '-3'
+     */
+    public function testMultipleNegative(){
+        $neg = "-1,-3,3";
+
+        $result = Kata::add($neg);
+    }
+
+    public function testAddAboveThousand(){
+        $two = "2,1001";
+        $this->assertEquals(2, Kata::add($two));
+    }
+
+    public function testAddWithLongCustomDelim(){
+        $five = "//[***]\n1***2***3";
+        $this->assertEquals(6, Kata::add($five));
+    }
+
+    public function testAddWithMultipleCustomDelim(){
+        $five = "//[*][%]\n1*2%3";
+        $this->assertEquals(6, Kata::add($five));
+    }
+
+    public function testAddWithMultipleLongCustomDelim(){
+        $five = "//[***][%]\n1***2%3";
+        $this->assertEquals(6, Kata::add($five));
+    }
+}
