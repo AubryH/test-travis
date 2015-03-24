665ce6b17b91ad4d8aa47ce7b0bb69ab6ac2b3ef
AubryH <aubryh@theodo.fr>
2015-03-24 18:22:45 +0100

    test with include

--- tests/KataTest.php
+++ tests/KataTest.php
@@ -6,9 +6,11 @@
  * Time: 10:20
  */
 
-require_once '../src/Kata.php';
-require_once '../src/Console.php';
-require_once '../src/Prompter.php';
++$dir = dirname(__FILE__);
++include "$dir/../src/Kata.php";
++include "$dir/../src/Console.php";
++include "$dir/../src/Prompter.php";
+
 
 class KataTest extends PHPUNIT_Framework_TestCase{
 
